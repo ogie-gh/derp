cc3b2e4a17f04dbf071feb5e745be65648315e07
ogie-gh <144860767+ogie-gh@users.noreply.github.com>
2023-09-15 20:50:33 +0930

    Update set_env.py

--- scripts/set_env.py
+++ scripts/set_env.py
@@ -4,4 +4,4 @@ def set_output(name, value):
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         print(f'{name}={value}', file=fh)
 
-set_output(issue, TST-3060)
+set_output("issue", "TST-3060")
